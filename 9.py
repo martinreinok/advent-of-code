ec84fe82abccf59309740fdb208fe2abbb3b4cc4
Martin Reinok <martin.reinok@gmail.com>
2021-12-09 21:20:38 +0200

    Day 9

--- 9.py
+++ 9.py
@@ -3,6 +3,8 @@ print(data9)
 low_points = []
 
 
+# Very messy solution, part 2 wasnt that bad
+
 def check_neighbours(datalist, checkrow, checkindex):
     if datalist[i + checkrow][n] > datalist[i][n] < datalist[i][n + checkindex]:
         return True
@@ -53,3 +55,20 @@ print("Low points:", low_points)
 print("Risk level:", sum(low_points) + len(low_points))
 
 # Part 2
+import numpy as np
+from scipy import ndimage
+from collections import Counter
+
+data9 = [[*map(int, x.strip())] for x in open(f"puzzle_input/9")]
+data9 = np.array(data9)
+data9[data9 < 9] = 1
+data9[data9 == 9] = 0
+# 0 is for background in scipy.ndimage.label (not considered)
+print(f"\n#Minesweeper\n {data9} \n")
+connected = ndimage.label(data9)
+print(connected)
+# connected_sizes = Counter(connected)  # nop
+unique, counts = np.unique(connected[0], return_counts=True)
+counts = np.sort(np.delete(counts, 0))  # delete 0 and sort
+print(counts)
+print("part2:", counts[-1] * counts[-2] * counts[-3])
